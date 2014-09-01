df358fdf07403e47dfa6c0d41894db9cdec589fe
Kwic05 <tugolav671410@yandex.ru>
2014-09-01 11:39:16 +0900

    autoindent, little bug, etc...

--- dlg4.cpp
+++ dlg4.cpp
@@ -48,10 +48,10 @@ void dlg4::on_pushButton_2_clicked()
     if (!db.open()){qDebug()<<"222";}
     QSqlQuery* qs_ = new QSqlQuery(db);
     qDebug()<<qs_->lastError().text();
-    qs_->exec("create table zxc (id int primary key, "
-                  "FIO varchar(20), pol varchar(20), dr varchar(20), kurenie varchar(20), id2 int, id3 int)");
-    qs_->exec("select * from zxc");
-    qs_->exec("UPDATE asd SET id3 = '0'");
+    qs_->exec("ALTER TABLE zxc ADD COLUMN id3 INT AFTER id2");
+    qs_->exec("UPDATE zxc SET id3 = '0'");
+    qs_->exec("select * from zxc order by id");
+
     int nNumber2=0;
     qDebug()<<nNumber2;
     qc_->executeSelect();
@@ -62,171 +62,183 @@ void dlg4::on_pushButton_2_clicked()
     qDebug()<<nNumber2 ;
     nNumber2 = nNumber2 + 1;
     qDebug()<<nNumber2;
-           QSqlRecord rec2  = qs_->record();
+    QSqlRecord rec2  = qs_->record();
 
-           //итератор
-           int nNumber22=0;
-           qDebug()<<nNumber22;
-           qc_->executeSelect();
-           if (qc_->query2.last()) {
-               QSqlRecord rec3_imp  = qc_->query2.record();
-               nNumber22 = nNumber22 + qc_->query2.value(rec3_imp.indexOf("id2")).toInt();
-           }
-           qDebug()<<nNumber22;
-           nNumber22 = nNumber22 + 1;
-           qDebug()<<nNumber22;
+    //итератор
+    int nNumber22=0;
+    qDebug()<<nNumber22;
+    qc_->executeSelect();
+    if (qc_->query2.last()) {
+        QSqlRecord rec3_imp  = qc_->query2.record();
+        nNumber22 = nNumber22 + qc_->query2.value(rec3_imp.indexOf("id2")).toInt();
+    }
+    qDebug()<<nNumber22;
+    nNumber22 = nNumber22 + 1;
+    qDebug()<<nNumber22;
 
 
     QSqlQuery* qm_ = new QSqlQuery(db);
 
-    qm_->exec("create table asd (mark varchar(20), id2 int primary key, "
-               "kol int), nikotin int, id3 int)");
-    qm_->exec("select * from asd");
+
+    qm_->exec("ALTER TABLE asd ADD COLUMN id3 INT AFTER nikotin");
     qm_->exec("UPDATE asd SET id3 = '0'");
+    qm_->exec("select * from asd order by id2");
+
+
 
     QSqlRecord rec_imp = qm_->record();
-           while (qm_->next()) {
-                        qDebug()<<"qm->";
+    QSqlRecord rec  = qc_->query2.record();
+    while (qc_->query2.previous()) {
+        qDebug()<<"qc_->";
 
-                QString strmark_imp  = qm_->value(rec_imp.indexOf("mark")).toString(),
-                        strid2_imp   = qm_->value(rec_imp.indexOf("id2")).toString();
 
-                while (qc_->query2.next()) {
-                     qDebug()<<"qm_-> qc_->";
-                    QSqlRecord rec  = qc_->query2.record();
+        QString strmark  = qc_->query2.value(rec.indexOf("mark")).toString(),
+                strid2  = qc_->query2.value(rec.indexOf("id2")).toString();
+        qDebug()<<strmark;
 
-                    QString strmark  = qc_->query2.value(rec.indexOf("mark")).toString(),
-                            strid2  = qc_->query2.value(rec.indexOf("id2")).toString();
 
-                    if (strmark_imp == strmark) {
-                          qDebug()<<"if1";
+
+        if (qm_->first()) {qm_->previous();}
+
+        while (qm_->next()) {
+            qDebug()<<"qm->";
+
+            QString strmark_imp  = qm_->value(rec_imp.indexOf("mark")).toString(),
+                    strid2_imp   = qm_->value(rec_imp.indexOf("id2")).toString(),
+                    strid3_imp   = qm_->value(rec_imp.indexOf("id3")).toString();
+            qDebug()<<strmark_imp;
+            int i=0;
+
+            if (strmark_imp == strmark) {
+
+                i=1;
+                qDebug()<<"if1";
+                QString e = "UPDATE asd"
+                        " SET id3 = '%1'"
+                        " WHERE id2 = '%2'";
+
+                QString f = "UPDATE zxc"
+                        " SET id3 = '%1'"
+                        " WHERE id2 = '%2'";
+
+                QString w;
+                w = e.arg(strid2)
+                        .arg(strid2_imp);
+
+                QString l;
+                l = f.arg(strid2)
+                        .arg(strid2_imp);
+                qDebug()<<l;
+                qDebug()<<w;
+
+                if (!qm_->exec(w)) {qDebug()<<w;}
+                if (!qs_->exec(l)) {qDebug()<<l;}
+                qm_->exec("select * from asd order by id2");
+                qs_->exec("select * from zxc order by id");
+            }
+            else
+            {
+
+                if (strid3_imp == "0") {
+                    if (i==0){
+                        qDebug()<<"if2";
                         QString e = "UPDATE asd"
-                                "SET id3 = '%1'"
-                                "WHERE id2 = '%2'";
-
-                        QString f = "UPDATE zxc"
-                                "SET id3 = '%1'"
-                                "WHERE id2 = '%2'";
-
-                        QString w;
-                        w = e.arg(strid2)
-                             .arg(strid2_imp);
-
-                        QString l;
-                        l = f.arg(strid2)
-                             .arg(strid2_imp);
-
-                        if (!qm_->exec(w)) {}
-                        if (!qs_->exec(l)) {}
-                    }
-                    else
-                    {
-                         qDebug()<<"if2";
-                        QString e = "UPDATE asd"
-                                "SET id3 = '%1'"
-                                "WHERE id2 = '%2'";
+                                " SET id3 = '%1'"
+                                " WHERE id2 = '%2'";
                         QString w;
                         w = e.arg(nNumber22)
-                             .arg(strid2_imp);
-                        if (!qm_->exec(w)) {}
+                                .arg(strid2_imp);
+                        qDebug()<<w;
+                        if (!qm_->exec(w)) {qDebug()<<w;}
 
                         QString f = "UPDATE zxc"
-                                "SET id3 = '%1'"
-                                "WHERE id2 = '%2'";
+                                " SET id3 = '%1'"
+                                " WHERE id2 = '%2'";
                         QString l;
                         l = f.arg(nNumber22)
-                             .arg(strid2_imp);
-                        if (!qm_->exec(l)) {}
+                                .arg(strid2_imp);
+                        qDebug()<<l;
+                        if (!qm_->exec(l)) {qDebug()<<l;}
                         nNumber22++;
 
-
+                        qm_->exec("select * from asd order by id2");
+                        qs_->exec("select * from zxc order by id");
                     }
                 }
             }
 
+        }
 
 
 
+    }
 
 
 
-
-
+    qDebug()<<"rabotaet";
 
     if (qm_->first()) {
 
         QString z = "INSERT INTO asd(mark, id2, kol, nikotin)"
-                      "VALUES('%1', '%2', '%3', '%4');";
+                "VALUES('%1', '%2', '%3', '%4');";
         QString x;
         x = z.arg(qm_->value(rec_imp.indexOf("mark")).toString())
-             .arg(qm_->value(rec_imp.indexOf("id3")).toString())
-             .arg(qm_->value(rec_imp.indexOf("kol")).toString())
-             .arg(qm_->value(rec_imp.indexOf("nikotin")).toString());
+                .arg(qm_->value(rec_imp.indexOf("id3")).toString())
+                .arg(qm_->value(rec_imp.indexOf("kol")).toString())
+                .arg(qm_->value(rec_imp.indexOf("nikotin")).toString());
         qDebug()<<x<<"2asdasa";
         if (!qc_->query2.exec(x)) {qDebug()<<qs_->lastError().text();}
 
     }
 
-  while (qm_->next()) {
+    while (qm_->next()) {
 
         QString z = "INSERT INTO asd(mark, id2, kol, nikotin)"
-                      "VALUES('%1', '%2', '%3', '%4');";
+                "VALUES('%1', '%2', '%3', '%4');";
         QString x;
         x = z.arg(qm_->value(rec_imp.indexOf("mark")).toString())
-             .arg(qm_->value(rec_imp.indexOf("id3")).toString())
-             .arg(qm_->value(rec_imp.indexOf("kol")).toString())
-             .arg(qm_->value(rec_imp.indexOf("nikotin")).toString());
+                .arg(qm_->value(rec_imp.indexOf("id3")).toString())
+                .arg(qm_->value(rec_imp.indexOf("kol")).toString())
+                .arg(qm_->value(rec_imp.indexOf("nikotin")).toString());
         qDebug()<<x<<"2asdasa";
         if (!qc_->query2.exec(x)) {qDebug()<<qs_->lastError().text();}
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     if (qs_->first()) {
 
 
-    QString z = "INSERT INTO zxc(id, FIO, pol, dr, kurenie, id2)"
-                  "VALUES('%1', '%2', '%3', '%4', '%5', '%6');";
-    QString x;
-    x = z.arg(nNumber2)
-         .arg(qs_->value(rec2.indexOf("FIO")).toString())
-         .arg(qs_->value(rec2.indexOf("pol")).toString())
-         .arg(qs_->value(rec2.indexOf("dr")).toString())
-         .arg(qs_->value(rec2.indexOf("kurenie")).toString())
-         .arg(qs_->value(rec2.indexOf("id3")).toString());
-    qDebug()<<x<<"2asdasa";
+        QString z = "INSERT INTO zxc(id, FIO, pol, dr, kurenie, id2)"
+                "VALUES('%1', '%2', '%3', '%4', '%5', '%6');";
+        QString x;
+        x = z.arg(nNumber2)
+                .arg(qs_->value(rec2.indexOf("FIO")).toString())
+                .arg(qs_->value(rec2.indexOf("pol")).toString())
+                .arg(qs_->value(rec2.indexOf("dr")).toString())
+                .arg(qs_->value(rec2.indexOf("kurenie")).toString())
+                .arg(qs_->value(rec2.indexOf("id3")).toString());
+        qDebug()<<x<<"2asdasa";
 
-    if (!qc_->query.exec(x)) {qDebug()<<qs_->lastError().text();}
-                    }   
+        if (!qc_->query.exec(x)) {qDebug()<<qs_->lastError().text();}
+    }
 
 
-while (qs_->next())
+    while (qs_->next())
     {
-    nNumber2 = nNumber2 + 1;
-    QString z = "INSERT INTO zxc(id, FIO, pol, dr, kurenie, id2)"
-                 "VALUES('%1', '%2', '%3', '%4', '%5', '%6');";
-    QString x;
-    x = z.arg(nNumber2)
-         .arg(qs_->value(rec2.indexOf("FIO")).toString())
-         .arg(qs_->value(rec2.indexOf("pol")).toString())
-         .arg(qs_->value(rec2.indexOf("dr")).toString())
-         .arg(qs_->value(rec2.indexOf("kurenie")).toString())
-         .arg(qs_->value(rec2.indexOf("id3")).toString());
-    qDebug()<<x;
+        nNumber2 = nNumber2 + 1;
+        QString z = "INSERT INTO zxc(id, FIO, pol, dr, kurenie, id2)"
+                "VALUES('%1', '%2', '%3', '%4', '%5', '%6');";
+        QString x;
+        x = z.arg(nNumber2)
+                .arg(qs_->value(rec2.indexOf("FIO")).toString())
+                .arg(qs_->value(rec2.indexOf("pol")).toString())
+                .arg(qs_->value(rec2.indexOf("dr")).toString())
+                .arg(qs_->value(rec2.indexOf("kurenie")).toString())
+                .arg(qs_->value(rec2.indexOf("id3")).toString());
+        qDebug()<<x;
 
-    if (!qc_->query.exec(x)) {qDebug()<<qs_->lastError().text();}
+        if (!qc_->query.exec(x)) {qDebug()<<qs_->lastError().text();}
 
     }
 }
